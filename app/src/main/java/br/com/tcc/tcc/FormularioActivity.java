46b26587cdd7a62535a5723e908152ffcf110e69
cesardias <cesar.diiasf@gmail.com>
2018-03-26 16:35:37 -0300

    26_03_Inclusao_de_ToolBar

--- app/src/main/java/br/com/tcc/tcc/FormularioActivity.java
+++ app/src/main/java/br/com/tcc/tcc/FormularioActivity.java
@@ -6,9 +6,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.zip.Inflater;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -17,15 +22,24 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-    Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
-    botaoSalvar.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(FormularioActivity.this, "Pet Cadastrado com Sucesso!", Toast.LENGTH_SHORT).show();
-            Intent vaiPralista = new Intent(FormularioActivity.this,  ListaPetsActivity.class);
-            startActivity(vaiPralista); // Aqui ele vai jogar pra lista
-        }
-    });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       MenuInflater inflater =  getMenuInflater();
+       inflater.inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                Toast.makeText(FormularioActivity.this, "Pet Cadastrado com Sucesso!", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
